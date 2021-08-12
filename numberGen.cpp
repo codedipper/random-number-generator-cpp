9c7fcdcf16d41eec7c158ea2bc5a3d7756f22448
Revvy <alternative.5575@gmail.com>
2021-08-12 09:38:45 -0400

    Support command line arguments.

--- numberGen.cpp
+++ numberGen.cpp
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sstream>
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
 	int min;
 	int max;
+	
+	if (argc == 3)
+	{
+		min = stoi(argv[1]);
+		max = stoi(argv[2]);
+		
+		printf("Minimum number: %s", argv[1]);
+		printf("\nMaximum number: %s", argv[2]);
+		
+		srand(time(NULL));
+		printf("\n\nThe number is: %i", rand() % (max - min + 1) + min);
+		
+		system("pause >nul");
+		return 0;
+	}
 	
 	printf("Enter the minimum number: ");
 	scanf("%i", &min);
